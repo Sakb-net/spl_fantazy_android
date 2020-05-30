5e33eba20f730c5b8945acf49bb497a52a633d37
dev_mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2020-05-30 13:10:40 +0200

    add head to head popup

--- app/src/main/java/com/sakb/spl/ui/headtoheadleague/CreateHeadToHeadLeagueFragment.kt
+++ app/src/main/java/com/sakb/spl/ui/headtoheadleague/CreateHeadToHeadLeagueFragment.kt
@@ -1,29 +1,37 @@
 package com.sakb.spl.ui.headtoheadleague
 
 import android.app.AlertDialog
-import android.content.DialogInterface
+import android.content.*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.databinding.DataBindingUtil
-
 import com.sakb.spl.R
 import com.sakb.spl.base.BaseFragment
 import com.sakb.spl.databinding.CreateHeadToHeadLeagueFragmentBinding
+import com.sakb.spl.databinding.NewLeagueCreatedDialogBinding
 import com.sakb.spl.utils.showConfirmationDialog
+import com.sakb.spl.utils.toast
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class CreateHeadToHeadLeagueFragment : BaseFragment() {
     override val viewModel by viewModel<CreateHeadToHeadLeagueViewModel>()
-    private lateinit var binding : CreateHeadToHeadLeagueFragmentBinding
+    private lateinit var binding: CreateHeadToHeadLeagueFragmentBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding  = DataBindingUtil.inflate(inflater, R.layout.create_head_to_head_league_fragment, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.create_head_to_head_league_fragment,
+            container,
+            false
+        )
         return binding.root
     }
 
@@ -39,13 +47,23 @@ class CreateHeadToHeadLeagueFragment : BaseFragment() {
         }
 
         binding.buttonSend.setOnClickListener {
-            openConfirmationDialog()
+            //openConfirmationDialog()
+            context?.joinNowDialog(R.drawable.ic_league_cup,
+                title = getString(R.string.league_created_title),
+                contentResID = R.string.content_join_desc,
+                price = getString(R.string.code_league),
+                code = "7gkipd",
+                positive = { dialog -> dialog?.dismiss() },
+                negative = { dialog -> dialog?.dismiss() })
         }
     }
 
     private fun openConfirmationDialog() {
-        context?.showConfirmationDialog(R.drawable.ic_done, "تم الانضمام بنجاح لدوري \" المحترفين\""){
-                dialog ->  dialog?.dismiss()
+        context?.showConfirmationDialog(
+            R.drawable.ic_done,
+            "تم الانضمام بنجاح لدوري \" المحترفين\""
+        ) { dialog ->
+            dialog?.dismiss()
         }
     }
 
@@ -58,9 +76,10 @@ class CreateHeadToHeadLeagueFragment : BaseFragment() {
         val options = option.toTypedArray()
 
         var selectedItem = 0
-        val builder = AlertDialog.Builder(requireContext(),R.style.MaterialThemeDialog)
+        val builder = AlertDialog.Builder(requireContext(), R.style.MaterialThemeDialog)
         builder.setTitle(getString(R.string.select_round))
-        builder.setSingleChoiceItems(options
+        builder.setSingleChoiceItems(
+            options
             , 0
         ) { _: DialogInterface, item: Int ->
             selectedItem = item
@@ -86,9 +105,10 @@ class CreateHeadToHeadLeagueFragment : BaseFragment() {
         val options = option.toTypedArray()
 
         var selectedItem = 0
-        val builder = AlertDialog.Builder(requireContext(),R.style.MaterialThemeDialog)
+        val builder = AlertDialog.Builder(requireContext(), R.style.MaterialThemeDialog)
         builder.setTitle(getString(R.string.max_team_num))
-        builder.setSingleChoiceItems(options
+        builder.setSingleChoiceItems(
+            options
             , 0
         ) { _: DialogInterface, item: Int ->
             selectedItem = item
@@ -103,6 +123,49 @@ class CreateHeadToHeadLeagueFragment : BaseFragment() {
         }
         builder.create()
         builder.show()
+    }
+
+    private fun Context.joinNowDialog(
+        @DrawableRes drawableRes: Int,
+        title: String,
+        contentResID: Int? = null,
+        price: String? = null,
+        code: String? = null,
+        positive: (dialog: AlertDialog?) -> Unit,
+        negative: (dialog: AlertDialog?) -> Unit
+    ) {
+        val binding = NewLeagueCreatedDialogBinding.inflate(LayoutInflater.from(this), null, false)
+        val alertDialog = AlertDialog.Builder(this).setView(binding.root).setCancelable(false)
+        val dialog = alertDialog.create()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        binding.dialogImage.setImageResource(drawableRes)
+        binding.dialogTextTitle.text = title
+        binding.dialogTextContent.text = contentResID?.let { this.getString(it) }
+        binding.dialogTextPrice.text = contentResID?.let { price }
+        binding.codeText.text = contentResID?.let { code }
+        binding.copyBtn.setOnClickListener {
+            copyToClipboard(binding.codeText.text)
+        }
+        binding.shareBtn.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, binding.codeText.text.toString())
+            }
+            startActivity(Intent.createChooser(intent, "Share via"))
+        }
+
+
+        binding.positiveButton.setOnClickListener { positive(dialog) }
+        binding.negativeBtn.setOnClickListener { negative(dialog) }
+        dialog.show()
+    }
+
+    fun Context.copyToClipboard(text: CharSequence) {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("label", text)
+        clipboard.setPrimaryClip(clip)
+        toast("Copied")
     }
 
 
