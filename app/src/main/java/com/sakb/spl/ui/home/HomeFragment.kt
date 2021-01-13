6e013f15862052ac942003c0f82dc918abed39e9
Mohammed <mohammedfarid@users.noreply.github.com>
2021-01-13 22:12:32 +0200

    Edit Fixture in Home Page

--- app/src/main/java/com/sakb/spl/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sakb/spl/ui/home/HomeFragment.kt
@@ -200,6 +200,7 @@ class HomeFragment : BaseFragment() {
                     headerFixturesTitlesList.add(this)
                     headerFixturesAdapter.submitList(headerFixturesTitlesList)
                     fixturesAdapter.submitList(this.matchGroup)
+                    footerFixturesAdapter.hideFooter(data?.data)
                 }
             }
         }
--- app/src/main/java/com/sakb/spl/ui/home/adapters/FooterFixtureAdapter.kt
+++ app/src/main/java/com/sakb/spl/ui/home/adapters/FooterFixtureAdapter.kt
@@ -1,14 +1,16 @@
 package com.sakb.spl.ui.home.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.sakb.spl.data.model.DataItem
 import com.sakb.spl.databinding.ItemHomeShowAllMatchesBinding
 
 class FooterFixtureAdapter : RecyclerView.Adapter<FooterFixtureAdapter.FooterViewHolder>() {
 
     var onClickListener: (() -> Unit)? = null
-
+    var data: List<DataItem?>? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         FooterViewHolder(
             ItemHomeShowAllMatchesBinding.inflate(
@@ -19,17 +21,24 @@ class FooterFixtureAdapter : RecyclerView.Adapter<FooterFixtureAdapter.FooterVie
         )
 
     override fun onBindViewHolder(holderHeader: FooterViewHolder, position: Int) {
-        holderHeader.bind()
+        holderHeader.bind(data)
     }
 
     override fun getItemCount() = HEADER
 
     class FooterViewHolder(
         private val binding: ItemHomeShowAllMatchesBinding,
-        private val onChooseTeamClickListener: (() -> Unit)?
+        private val onChooseTeamClickListener: (() -> Unit)?,
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind() {
+        fun bind(data: List<DataItem?>?) {
+            data?.let { list ->
+                if (list.isEmpty()) {
+                    binding.cardFooter.visibility = View.GONE
+                } else {
+                    binding.cardFooter.visibility = View.VISIBLE
+                }
+            }
             itemView.run {
                 setOnClickListener {
                     onChooseTeamClickListener?.invoke()
@@ -40,6 +49,11 @@ class FooterFixtureAdapter : RecyclerView.Adapter<FooterFixtureAdapter.FooterVie
 
     fun onClear() {
         onClickListener = null
+    }
+
+    fun hideFooter(dataSend: List<DataItem?>?) {
+        data = dataSend
+        notifyDataSetChanged()
     }
 
     companion object {
